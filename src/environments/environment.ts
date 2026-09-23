6272da8ddaaa428f9f3ba363e0c5a495f5be5921
Johann Graneros <granerosjohann05@gmail.com>
2026-09-23 20:30:15 -0300

    chore(deploy): incluir configuración pública de Supabase

---
+++ src/environments/environment.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  supabase: {
+    Url: 'https://ripzuybckwrflwxbpftw.supabase.co',
+    Key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpcHp1eWJja3dyZmx3eGJwZnR3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODkyNTUxOTksImV4cCI6MjEwNDgzMTE5OX0.80HTaOgkiDhWDnxQ95FNq2WYcWWu0z2GpxtncAGNPk4'
+  }
+};
